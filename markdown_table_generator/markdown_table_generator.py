8d935558bd722c8ee85da793cbea22af39395748
Gilbert Montague <gilbert.i.montague@gmail.com>
2016-12-02 19:02:45 -0800

    First

---
+++ markdown_table_generator/markdown_table_generator.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""A simple application to read a text file and output a markdown table
+
+
+Example:
+        $ python markdown_table_generator --file my_file.txt --output my_table.md
+
+Attributes:
+
+Todo:
+
+"""
+import argparse
